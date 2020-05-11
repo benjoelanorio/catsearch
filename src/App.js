d01167e19421c872f0d5db7b0a5195f05af0f02f
Benjoe Lanorio <benjoelanorio@gmail.com>
2020-05-11 23:15:37 +0800

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.components';
+
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchFeild: ''
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  // handleChange(e) {
+  //   this.setState({ searchFeild: e.target.value })
+  // }
+
+  handleChange = (e) => {
+    this.setState({ searchFeild: e.target.value })
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+      .catch(err => console.log('Something went Wrong'));
+  }
+
+
+  render() {
+    const { monsters, searchFeild } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchFeild.toLowerCase()));
+    return (
+      <div className="App">
+        <h1>Meow Search</h1>
+        <SearchBox
+          placeholder='search cats'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    )
+  }
 }
 
 export default App;

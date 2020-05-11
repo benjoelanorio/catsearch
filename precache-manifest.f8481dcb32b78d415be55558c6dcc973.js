c08f6232a5dde33d9162ed5e04df0a601b903acb
Benjoe Lanorio <benjoelanorio@gmail.com>
2020-05-11 23:13:33 +0800

    Updates

---
+++ precache-manifest.f8481dcb32b78d415be55558c6dcc973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc13aab600dadb62065848c709e41708",
+    "url": "/catsearch/index.html"
+  },
+  {
+    "revision": "7ed84e8a8eda7a9eefc7",
+    "url": "/catsearch/static/css/main.693ce7d6.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/catsearch/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/catsearch/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed84e8a8eda7a9eefc7",
+    "url": "/catsearch/static/js/main.6f3d55c4.chunk.js"
+  },
+  {
+    "revision": "16e37f59a16d37520770",
+    "url": "/catsearch/static/js/runtime-main.22576802.js"
+  }
+]);

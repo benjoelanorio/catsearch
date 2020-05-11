f9ea56d5358237eb3d62be5b949bf9f05582edbf
Benjoe Lanorio <benjoelanorio@gmail.com>
2020-05-11 23:43:06 +0800

    t

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch(`https://jsonplaceholder.typicode.com/users`)
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }))
       .catch(err => console.log('Something went Wrong'));
